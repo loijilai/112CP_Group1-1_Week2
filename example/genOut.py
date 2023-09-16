0b3f0cbdc1a8d3789be07566ab85b71186c83bcf
loijilai <loijilai@gmail.com>
2023-09-16 20:49:04 +0800

    Add .out

--- example/genOut.py
+++ example/genOut.py
@@ -1,14 +1,14 @@
 import os
 
 c = [e for e in os.listdir() if e.endswith('.c')][0]
-os.system('gcc '+c+' -lm '+' -o '+)
+os.system('gcc '+c+' -o '+ c.replace('.c', '.gg')+' -lm ')
 
 exe = [e for e in os.listdir() if e.endswith('.gg')][0]
 
 ins = [e for e in os.listdir() if e.endswith('.in')]
 
 for ine in ins:
-    os.system(exe+' < '+ine+' > '+ine.replace('.in','.out'))
+    os.system('./'+exe+' < '+ine+' > '+ine.replace('.in','.out'))
 
 for i,ine in enumerate(ins):
     print('----------------------------')
@@ -19,4 +19,4 @@ for i,ine in enumerate(ins):
     print('----------------------------')
     with open(ine.replace('.in','.out'),encoding='utf-8') as f:
         print(f.read())
-#input('press enter to continue')
+input('press enter to continue')
